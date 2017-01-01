0cb56bae4e1e0df862624e440653e482580c8069
Alexey Melezhik <melezhik@gmail.com>
2017-01-01 20:54:04 +0300

    ability to pass env vars

---
+++ modules/bash-command/story.bash
@@ -0,0 +1,20 @@
+user=$(config user)
+command=$(config command)
+debug=$(config debug)
+envvars=$(story_var envvars)
+
+if test $debug -eq 1; then
+  set -x;
+fi
+
+#if  test ! -z "${envvars}"; then
+#  echo $envvars
+#fi
+
+if test -z $user; then
+  bash -c "${envvars} ${command}" || exit 1
+else
+  su --shell `which bash` --login -c "${envvars} ${command}" $user || exit 1
+fi
+
+
